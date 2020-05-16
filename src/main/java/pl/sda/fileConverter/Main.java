620e0ad6331dd912e06703571cee7254b1e07002
f_kazmierczak <kazmierczakf@gmail.com>
2020-05-16 10:26:11 +0200

    add json file

--- src/main/java/pl/sda/fileConverter/Main.java
+++ src/main/java/pl/sda/fileConverter/Main.java
@@ -2,6 +2,10 @@ package pl.sda.fileConverter;
 
 public class Main {
     public static void main(String[] args) {
+        Converter converter = new Converter();
+        String sourceFilePath = "test.json";
+        String outputFilePath = "test.xlsx";
 
+        converter.convert(sourceFilePath,outputFilePath);
     }
 }
---
+++ src/main/java/pl/sda/fileConverter/Reader.java
@@ -0,0 +1,10 @@
+package pl.sda.fileConverter;
+
+import java.util.List;
+import java.util.Map;
+
+public class Reader {
+    public static List<Map> read(String inputFilePath){
+        return null;
+    }
+}
